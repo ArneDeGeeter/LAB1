d62edbe1458112af3d0f3c9f0a50ac51c65eb83d
Unknown <maes_andres@hotmail.com>
2019-03-07 18:20:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/sample/newTabController.java
+++ src/sample/newTabController.java
@@ -3,8 +3,6 @@ package sample;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -49,6 +47,19 @@ public class newTabController {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 if (choiceBox.getItems().get((Integer) newValue).equals("First come, first served")) {
+                    button.setLayoutX(7);
+                    button.setLayoutY(104 + 2 * 26);
+                    button.setDisable(false);
+                    button.setText("Run algorithm");
+
+                    button.setOnAction(e -> {
+                        FCFSScheduler fcfsScheduler = new FCFSScheduler(fileToArraylist(file));
+                        fcfsScheduler.startScheduling();
+                        GraphData.generatePercentileGraph(fcfsScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(fcfsScheduler);
+                    });
+
+                    anchor.getChildren().add(button);
 
                 } else if (choiceBox.getItems().get((Integer) newValue).equals("Round robin")) {
                     button.setLayoutX(7);
@@ -84,21 +95,125 @@ public class newTabController {
                     button.setOnAction(e -> {
                         RRScheduler rrScheduler = new RRScheduler(fileToArraylist(file),Integer.parseInt(txtfield.getText()));
                         rrScheduler.startScheduling();
-
-                        GraphData.generateGraph(rrScheduler);
+                        GraphData.generatePercentileGraph(rrScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(rrScheduler);
                     });
 
                     anchor.getChildren().add(txtfield);
                     anchor.getChildren().add(lbl);
                     anchor.getChildren().add(button);
                 } else if (choiceBox.getItems().get((Integer) newValue).equals("Shortest process next")) {
+                    button.setLayoutX(7);
+                    button.setLayoutY(104 + 2 * 26);
+                    button.setDisable(false);
+                    button.setText("Run algorithm");
 
+                    button.setOnAction(e -> {
+                        SPNScheduler spnScheduler = new SPNScheduler(fileToArraylist(file));
+                        spnScheduler.startScheduling();
+                        GraphData.generatePercentileGraph(spnScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(spnScheduler);
+                    });
+
+                    anchor.getChildren().add(button);
                 } else if (choiceBox.getItems().get((Integer) newValue).equals("Shortest time remaining")) {
+                    button.setLayoutX(7);
+                    button.setLayoutY(104 + 2 * 26);
+                    button.setDisable(false);
+                    button.setText("Run algorithm");
 
+                    button.setOnAction(e -> {
+                        STRScheduler strScheduler = new STRScheduler(fileToArraylist(file));
+                        strScheduler.startScheduling();
+                        GraphData.generatePercentileGraph(strScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(strScheduler);
+                    });
+
+                    anchor.getChildren().add(button);
                 } else if (choiceBox.getItems().get((Integer) newValue).equals("Highest response rate next")) {
+                    button.setLayoutX(7);
+                    button.setLayoutY(104 + 2 * 26);
+                    button.setDisable(false);
+                    button.setText("Run algorithm");
 
+                    button.setOnAction(e -> {
+                        HRRNScheduler hrrnScheduler = new HRRNScheduler(fileToArraylist(file));
+                        hrrnScheduler.startScheduling();
+                        GraphData.generatePercentileGraph(hrrnScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(hrrnScheduler);
+                    });
+
+                    anchor.getChildren().add(button);
                 } else if (choiceBox.getItems().get((Integer) newValue).equals("Multi level feedback")) {
+                    button.setLayoutX(7);
+                    button.setLayoutY(200);
+                    button.setDisable(false);
+                    button.setText("Run algorithm");
+                    TextField string = new TextField();
+                    string.setLayoutX(7);
+                    string.setLayoutY(155);
+                    string.textProperty().addListener(new ChangeListener<String>() {
+                        @Override
+                        public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                            String newValue) {
+                            if (!newValue.matches("\\d*")) {
+                                string.setText(newValue.replaceAll("[^\\d]", ""));
+                            }
+                            if (!newValue.trim().isEmpty()) {
+                                number = true;
+                                button.setDisable(!number || !fileb);
 
+                            } else {
+                                number = false;
+                                button.setDisable(!number || !fileb);
+                            }
+                            System.out.println(newValue);
+                        }
+                    });
+
+                    TextField txtfield = new TextField();
+                    txtfield.setLayoutX(7);
+                    txtfield.setLayoutY(104);
+                    txtfield.textProperty().addListener(new ChangeListener<String>() {
+                        @Override
+                        public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                            String newValue) {
+                            if (!newValue.matches("\\d*")) {
+                                txtfield.setText(newValue.replaceAll("[^\\d]", ""));
+                            }
+                            if (!newValue.trim().isEmpty()) {
+                                number = true;
+                                button.setDisable(!number || !fileb);
+
+                            } else {
+                                number = false;
+                                button.setDisable(!number || !fileb);
+                            }
+                            System.out.println(newValue);
+                        }
+                    });
+                    Label lbl1 = new Label();
+                    lbl1.setLayoutX(7);
+                    lbl1.setLayoutY(78);
+                    lbl1.setText("Choose max time");
+
+                    Label lbl2 = new Label();
+                    lbl2.setLayoutX(7);
+                    lbl2.setLayoutY(130);
+                    lbl2.setText("give string");
+
+                    button.setOnAction(e -> {
+                        MLFBScheduler mlfbScheduler = new MLFBScheduler(fileToArraylist(file), string.getText(),Integer.parseInt(txtfield.getText()));
+                        mlfbScheduler.startScheduling();
+                        GraphData.generatePercentileGraph(mlfbScheduler);
+                        GraphData.generateRelativeServiceTimeGraph(mlfbScheduler);
+                    });
+
+                    anchor.getChildren().add(txtfield);
+                    anchor.getChildren().add(lbl1);
+                    anchor.getChildren().add(string);
+                    anchor.getChildren().add(lbl2);
+                    anchor.getChildren().add(button);
                 }
             }
 
